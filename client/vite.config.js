9906d5b5b1aba7a76625d523a1784a481ae33c49
jitutanwar123 <jatinsinghtanwar783@gmail.com>
2026-06-26 10:37:59 +0530

    updatee

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+    preview: {
+    allowedHosts: ['sunny-luck-production-b1f0.up.railway.app'],
+    host: '0.0.0.0',
+    port: 8080,
+  },
   server: {
     port: 5173,
     proxy: {
